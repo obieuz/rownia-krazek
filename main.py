9a5d5874b40fa9ba7379b4aaac31138ac99728ec
obieuz <robizaj2006@gmail.com>
2024-03-08 01:20:42 +0100

    rownia v2

--- main.py
+++ main.py
@@ -3,35 +3,18 @@ import numpy as np
 import pygame_widgets
 from pygame_widgets.slider import Slider
 
-#TODO
-#- additional, zrobic 3 krazki które beda mialy kolizje i taka gre jak to z lodem
-
 print("Witaj w curringu w pythonie!!!")
 print("Kliknij d aby wypuścić krążek")
-print("Kliknij g aby wygenerowac nowa tarcze")
+print("Kliknij r aby zrestartowac")
 print("Do zobaczenia na pytońskim boisku")
 
-h=50
+h=80
 l=200
 m=1
 g=10
 f=0.2
 
-windowWidth=800
-windowHeight=600
-screen=pygame.display.set_mode((windowWidth,windowHeight))
 gapY=1
-
-krazekWidth=30
-krazekHeight=10
-
-redLength=5
-orangeLength=30
-yellowLength=60
-redBorder=[]
-orangeBorder=[]
-yellowBorder=[]
-spawnProcetion=100
 
 white=(255,255,255)
 black=(0,0,0)
@@ -40,30 +23,91 @@ orange=(255,140,0)
 yellow=(255,255,0)
 
 run=True
-kat=np.arctan((h+gapY)/l)
 
-def generujPlansze():
-    center = np.random.randint(l + spawnProcetion + redLength + orangeLength + yellowLength + krazekWidth,
-                               windowWidth - redLength - orangeLength - yellowLength - krazekWidth)
-    redBorder = [center - redLength - krazekWidth, center + redLength + krazekWidth]
-    orangeBorder = [redBorder[0]-orangeLength, redBorder[1]+orangeLength]
-    yellowBorder = [orangeBorder[0] - yellowLength, orangeBorder[1] + yellowLength]
-    return redBorder,orangeBorder,yellowBorder
-def rysujPlansze():
-    pygame.draw.line(screen,yellow,(yellowBorder[0],windowHeight-1),(yellowBorder[1],windowHeight-1),2)
-    pygame.draw.line(screen, orange, (orangeBorder[0],windowHeight-1), (orangeBorder[1], windowHeight-1),2)
-    pygame.draw.line(screen, red, (redBorder[0], windowHeight-1), (redBorder[1], windowHeight-1),2)
+class plansza:
+    def __init__(self,width,height,krazekWidth,krazekHeight,spawnProcetion,redWidth,orangeWidth,yellowWidth):
+        self.windowWidth=width
+        self.windowHeight=height
+        self.krazekWidth=krazekWidth
+        self.krazekHeight=krazekHeight
+        self.spawnProcetion=spawnProcetion
+        self.screen=pygame.display.set_mode((width,height))
+        self.kat=np.arctan((h + gapY) / l)
+        self.excist=False
 
-def rysujPunkty():
-    punkty=k1.zdobytePunkty
-    pygame.draw.rect(screen, black, (windowWidth - 50, 0, 50, 30))
-    text = font.render(str(punkty), True, white, black)
-    textRect = text.get_rect()
-    textRect.center = (windowWidth - 40, 20)
-    screen.blit(text, textRect)
+        self.redBorder=[]
+        self.redWidth=redWidth
+        self.orangeBorder = []
+        self.orangeWidth=orangeWidth
+        self.yellowBorder = []
+        self.yellowWidth=yellowWidth
 
+        self.Krazki=[]
+        self.currentKrazek=-1
+
+        self.punkty=0
+
+    def generujPlansze(self):
+        center = np.random.randint(l + self.spawnProcetion + self.redWidth + self.orangeWidth + self.yellowWidth + self.krazekWidth,self.windowWidth - self.redWidth - self.orangeWidth - self.yellowWidth - self.krazekWidth)
+        self.redBorder = [center - self.redWidth - self.krazekWidth, center + self.redWidth + self.krazekWidth]
+        self.orangeBorder = [self.redBorder[0]-self.orangeWidth, self.redBorder[1]+self.orangeWidth]
+        self.yellowBorder = [self.orangeBorder[0] - self.yellowWidth, self.orangeBorder[1] + self.yellowWidth]
+
+    def obliczKat(self):
+        self.kat=np.arctan((h + gapY) / l)
+    def Punkty(self):
+        pygame.draw.rect(self.screen, black, (self.windowWidth - 50, 0, 50, 30))
+        text = font.render(str(self.punkty), True, white, black)
+        textRect = text.get_rect()
+        textRect.center = (self.windowWidth - 40, 20)
+        self.screen.blit(text, textRect)
+
+    def Plansza(self):
+        pygame.draw.line(self.screen, yellow, (self.yellowBorder[0], self.windowHeight - 1), (self.yellowBorder[1], self.windowHeight - 1), 2)
+        pygame.draw.line(self.screen, orange, (self.orangeBorder[0], self.windowHeight - 1), (self.orangeBorder[1], self.windowHeight - 1), 2)
+        pygame.draw.line(self.screen, red, (self.redBorder[0], self.windowHeight - 1), (self.redBorder[1], self.windowHeight - 1), 2)
+
+    def Update(self):
+        self.Krazki[self.currentKrazek].y=self.windowHeight-h
+
+    def Kolizje(self):
+        for i in range (self.currentKrazek+1):
+            for j in range (self.currentKrazek+1):
+                if i==j:
+                    break
+                if self.Krazki[i].x+self.Krazki[i].width>=self.Krazki[j].x and self.Krazki[i].x - 1<=self.Krazki[j].x+self.Krazki[j].width:
+                    vf=self.Krazki[i].v/(self.Krazki[i].m+self.Krazki[j].m)
+                    self.Krazki[i].v=vf
+                    self.Krazki[j].v=vf
+    def Rysuj(self):
+        self.screen.fill(black)
+        self.obliczKat()
+        if self.excist:
+            self.Kolizje()
+        pygame.draw.polygon(self.screen, white,
+                            [(0, self.windowHeight - gapY), (0 + l, self.windowHeight - gapY), (0, self.windowHeight - h - gapY)], 1)
+        self.Punkty()
+        self.Plansza()
+        for i in range (self.currentKrazek+1):
+            self.Krazki[i].ruchKrazek(self.windowHeight, self.kat)
+            self.Krazki[i].rysujKrazek(self.screen,self.windowHeight,self.kat)
+
+        pygame.display.update()
+
+    def start(self):
+        self.excist=False
+        self.currentKrazek=-1
+        self.punkty=0
+        self.Krazki = []
+        self.generujPlansze()
+        for i in range (3):
+            self.Krazki.append(krazek(0,self.windowHeight-h,self.krazekWidth,self.krazekHeight,white))
+
+    def sprawdz(self):
+        for i in range (3):
+            self.punkty+=self.Krazki[i].sprawdzKrazek(self.redBorder,self.orangeBorder,self.yellowBorder)
 class krazek:
-    def __init__(self,width,height,x,y,color):
+    def __init__(self,x,y,width,height,color):
         self.width=width
         self.height=height
         self.x=x
@@ -71,79 +115,71 @@ class krazek:
         self.color=color
         self.m=m
         self.v=0
-        self.stopped=False
-        self.checked=False
-        self.zdobytePunkty=0
-    def rysuj(self):
-        screen.fill((0,0,0))
-        pygame.draw.polygon(screen, white, [(0, windowHeight-gapY), (0 + l, windowHeight-gapY), (0, windowHeight - h-gapY)], 1)
-        rysujPlansze()
-        rysujPunkty()
-        if self.y >= windowHeight -2:
+    def rysujKrazek(self,screen,windowHeight,kat):
+        if self.y >= windowHeight - 2:
             pygame.draw.rect(screen, white, (self.x, self.y - self.height, self.width, self.height))
         else:
-            pygame.draw.polygon(screen, white, [(self.x, self.y - gapY), (self.x, self.y - self.height - gapY), (
-                self.x + self.width * np.cos(kat), self.y + self.width * np.sin(kat) - self.height - gapY), (
+            pygame.draw.polygon(screen, white, [(self.x, self.y - gapY), (self.x+self.height*np.sin(kat), self.y - self.height - gapY), (
+                self.x + self.width * np.cos(kat)+self.height*np.sin(kat), self.y + self.width * np.sin(kat) - self.height - gapY), (
                                                     self.x + self.width * np.cos(kat),
                                                     self.y + self.width * np.sin(kat) - gapY)])
-        pygame.display.update()
 
-    def sprawdz(self):
+    def sprawdzKrazek(self,redBorder,orangeBorder,yellowBorder):
         punkt=0
         if((self.x>=redBorder[0] and self.x+1<=redBorder[1]) or (self.x+self.width>=redBorder[0] and self.x+self.width-1<=redBorder[1])):
-            print("czerwone")
+            print("czerwone!")
             punkt=3
         elif((self.x>=orangeBorder[0] and self.x+1<=orangeBorder[1]) or (self.x+self.width>=orangeBorder[0] and self.x+self.width-1<=orangeBorder[1])):
-            print("pomaranczowe")
+            print("pomaranczowe!")
             punkt=2
         elif((self.x>=yellowBorder[0] and self.x+1<=yellowBorder[1]) or (self.x+self.width>=yellowBorder[0] and self.x+self.width-1<=yellowBorder[1])):
-            print("zolte")
+            print("zolte!")
             punkt=1
-        self.checked=True
-        self.zdobytePunkty+=punkt
-    def przesunKrazek(self):
-        self.rysuj()
-        if(self.checked):
-            return
-        elif(self.stopped):
-            self.sprawdz()
-            return
-        else:
-            if (self.y >= windowHeight - 2):
-                self.v -= (f*m*g)/m
-                if (self.v < 0):
-                    self.stopped=True
-                    return
+        return punkt
+    def ruchKrazek(self,windowHeight,kat):
+        if (self.y >= windowHeight - 2):
+            self.v -= (f * m * g) / m
+            if not self.v < 0:
                 self.y = windowHeight - 2
-                self.x+=self.v
-            else:
-                self.v += (m * g * np.sin(kat) - (f * m * g)) / m
-                self.x = self.x + self.v * np.cos(kat)
-                self.y=self.y+self.v*np.sin(kat)
+                self.x += self.v
+        else:
+            self.v += (m * g * np.sin(kat) - (f * m * g)) / m
+            self.x = self.x + self.v * np.cos(kat)
+            self.y = self.y + self.v * np.sin(kat)
 pygame.init()
 
-slider = Slider(screen, 20, 20, 100, 20, min=45, max=160, step=5, initial=50)
 font=pygame.font.Font('freesansbold.ttf',16)
 
-redBorder,orangeBorder,yellowBorder=generujPlansze()
-k1 = krazek(krazekWidth,krazekHeight,0,windowHeight-h,white)
+gameboard = plansza(800,600,30,15,100,15,30,60)
 
+slider = Slider(gameboard.screen, 20, 20, 100, 20, min=45, max=160, step=5, initial=80)
+gameboard.start()
+legal=True
 while run:
     events=pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             run=False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-            k1.x=0
-            k1.y=windowHeight-h
-            k1.v=0
-            k1.stopped=False
-            k1.checked=False
-            kat = np.arctan((h + gapY) / l)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_g:
-            redBorder,orangeBorder,yellowBorder=generujPlansze()
-    k1.przesunKrazek()
+            gameboard.currentKrazek += 1
+            gameboard.excist=True
+            if not gameboard.currentKrazek == 3:
+                gameboard.Update()
+            if gameboard.currentKrazek > 2:
+                if legal:
+                    gameboard.sprawdz()
+                    legal=False
+                gameboard.currentKrazek -= 1
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            gameboard.start()
+    gameboard.Rysuj()
+    if(not legal):
+        pygame.time.delay(3000)
+        gameboard.start()
+        legal=True
     h=int(slider.getValue())
+    gameboard.obliczKat()
     pygame_widgets.update(events)
     pygame.display.update()
     pygame.time.delay(100)
